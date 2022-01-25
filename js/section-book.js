780063e93cc70ce04e4e3e527770fab11083cf48
lsw04521 <lsw04521@naver.com>
2022-01-25 17:35:36 +0900

    22-01-25

--- js/section-book.js
+++ js/section-book.js
@@ -1,11 +1,19 @@
 $(function () {
 
     $.get("./sub_txt/section-1.txt", function (data) {
-
         $("#section-1").html(data);
+    })
 
+    $.get("./sub_txt/section-2.txt", function (data) {
+        $("#section-2").html(data);
+    })
 
+    $.get("./sub_txt/section-3.txt", function (data) {
+        $("#section-3").html(data);
+    })
 
+    $.get("./sub_txt/section-4.txt", function (data) {
+        $("#section-4").html(data);
     })
 
 
